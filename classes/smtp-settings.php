ddbd725a2804d825e76e4d40819368a607985c2d
Abu Sohib <abushoaib73@gmail.com>
2020-09-22 20:55:13 +0600

    Update class writig method

--- classes/smtp-settings.php
+++ classes/smtp-settings.php
@@ -1,14 +1,14 @@
 <?php
 
- class smtp_setting {
+ class smtpSetting {
 
-    const SMTP_SETTINGS = 'kau-smtp-settings';
+    const SMTP_SETTINGS = 'kau_smtp_settings';
 
-    public static function save_smtp_settings($value) {
+    public static function save($value) {
              update_option(self::SMTP_SETTINGS, json_encode($value));
     }
 
-    public static function get_smtp_settings() {
+    public static function get() {
 
         $smtpSettings = json_decode(get_option(self::SMTP_SETTINGS), true);
             if (is_array($smtpSettings)) {
