0419177170d5f26d53fe6982c21495460a1f49cb
kauniaweb <70848865+kauniaweb@users.noreply.github.com>
2020-09-28 17:37:38 +0600

    js and css file moved to assets folder

--- free-wp-smtp.php
+++ free-wp-smtp.php
@@ -28,20 +28,20 @@ function smtp_admin_styles() {
    
     if ('toplevel_page_smtpstartup' === $screen->id) {
         wp_enqueue_style('bootstrap4', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css');
-        wp_enqueue_style('smtp_startup_admin_css', plugins_url('css/startup.css', __FILE__), array(), '1.0');
+        wp_enqueue_style('smtp_startup_admin_css', plugins_url('assets/css/startup.css', __FILE__), array(), '1.0');
         
-        wp_enqueue_script( 'wpsmtp_admin_js', plugins_url('js/smtp-settings.js', __FILE__), array(), '1.0');
+        wp_enqueue_script( 'wpsmtp_admin_js', plugins_url('assets/js/smtp-settings.js', __FILE__), array(), '1.0');
         wp_enqueue_script( 'bootjquery','https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js', array( 'jquery' ),'',true );
 
       
     } elseif('free-wp-smtp_page_smtp_settings' === $screen->id) {
        // echo "<script type='text/javascript'>alert('worked');</script>";
-      wp_enqueue_script( 'wpsmtp_admin_js', plugins_url('js/smtp-settings.js', __FILE__), array(), '1.0');
+      wp_enqueue_script( 'wpsmtp_admin_js', plugins_url('assets/js/smtp-settings.js', __FILE__), array(), '1.0');
       wp_enqueue_script( 'bootjquery','https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js', array( 'jquery' ),'',true );  
       wp_enqueue_script( 'bootjquery2','https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js', array( 'jquery' ),'',true );
       
 
-      wp_enqueue_style('smtp_settings_admin_css', plugins_url('css/smtp-settings-style.css', __FILE__), array(), '1.0');
+      wp_enqueue_style('smtp_settings_admin_css', plugins_url('assets/css/smtp-settings-style.css', __FILE__), array(), '1.0');
       wp_enqueue_style('bootstrap4', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
       wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css');
     }
